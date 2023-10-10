90bb4f5ead385629006eeecb6f30e0ada87a1f04
BrunoCOliveira37 <brunochavesdeoliveira2003@gmail.com>
2023-10-10 00:17:08 -0300

    Adicionando sistema de crítico e o dano flutuando
    Adicionei um script para o Jogador ter noção do dano que está dando, nele o dano é mostrado saindo direto do inimigo/jogador(em caso de perda de vida). a fim de tornar esse script melhor implementei um sistema de crítico (que pode modificar a chance de crítico, para que no futuro a implementação de um power up de crítico seja possível), nele o player tem uma chance de crítico e em caso de acerto, o dano padrão da arma é multiplicado por um random de range diferente dependendo da arma

--- Assets/Scripts/Weapons/Rocket.cs
+++ Assets/Scripts/Weapons/Rocket.cs
@@ -5,12 +5,167 @@ using UnityEngine;
 public class Rocket : BulletFather
 {
     bool isExploded;
+    private int critic;
+
+    public int criticChance;
+
     void Start()
     {
         isRocket = true;
         rig = GetComponent<Rigidbody2D>();
     }
-
+    void critico()
+    {
+        critic = Random.Range(1, 100);
+        if (criticChance == 5)
+        {
+            if (critic <= 5)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 10)
+        {
+            if (critic <= 10)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 15)
+        {
+            if (critic <= 15)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 20)
+        {
+            if (critic <= 20)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 25)
+        {
+            if (critic <= 25)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 30)
+        {
+            if (critic <= 30)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 35)
+        {
+            if (critic <= 35)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 40)
+        {
+            if (critic <= 40)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 45)
+        {
+            if (critic <= 45)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 50)
+        {
+            if (critic <= 50)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 55)
+        {
+            if (critic <= 55)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 60)
+        {
+            if (critic <= 60)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 65)
+        {
+            if (critic <= 65)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 70)
+        {
+            if (critic <= 70)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 75)
+        {
+            if (critic <= 75)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 80)
+        {
+            if (critic <= 80)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 85)
+        {
+            if (critic <= 85)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 90)
+        {
+            if (critic <= 90)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 95)
+        {
+            if (critic <= 95)
+            {
+                critic = 1;
+            }
+        }
+        if (criticChance == 100)
+        {
+            if (critic <= 100)
+            {
+                critic = 1;
+            }
+        }
+        if (critic == 1)
+        {
+            damage = damage * Random.Range(2, 5);
+        }
+    }
+    void resetar()
+    {
+        damage = 5;
+    }
 
     void FixedUpdate()
     {
@@ -22,18 +177,21 @@ public class Rocket : BulletFather
         {
             if (collision.gameObject.GetComponent<LifeSystem>())
             {
+                critico();
                 collision.gameObject.GetComponent<LifeSystem>().OnDamage(damage);
+                resetar();
             }
             if (!isExploded)
             {
                 rig.velocity = Vector2.zero;
                 GetComponent<Animator>().Play("RocketExplosion");//O foguete ira explodir a partir dessa animacao
                 isExploded = true;//Para garantir que ele exploda apenas uma vez
-                damage = (int)(damage * 0.75f);//E quem for pego na explosao, vai perder menos vida que quem foi atingido pelo foguete
+                damage = (int)(damage * 0.5f);//E quem for pego na explosao, vai perder menos vida que quem foi atingido pelo foguete
             }
             
         }
     }
+    
     public void DeleteRocket()//Chamado no AnimationEvent, no fim da animacao da explosao
     {
         Destroy(gameObject);
